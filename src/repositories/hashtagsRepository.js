5e5c9f15a1a6bcbf0e2da2997d5c7ca1e9cd79ef
Flávia Bulad <flavia.bulad@gmail.com>
2022-08-13 22:45:32 -0300

    add get hashtags posts and fix: typos

--- src/repositories/hashtagsRepository.js
+++ src/repositories/hashtagsRepository.js
@@ -20,12 +20,25 @@ async function setUseCount(hashtagId) {
   return db.query(`UPDATE hashtags SET "useCount" = "useCount" + 1 WHERE id = 1$`, [hashtagId]);
 }
 
-async function deleteHashtag(id) {
-  return db.query(`DELETE FROM hashtags WHERE id = $1`, [id]);
-}
-
 async function insertHashtagPostRelation(postId, hashtagId) {
   return db.query(`INSERT INTO "hashtagsPosts"  ("postId", "hashtagId") VALUES ($1, $1)`, [postId, hashtagId]);
+}
+
+async function getHashtagPostByName(hashtag) {
+  return db.query(
+    `
+  SELECT p.id, json_build_object('name', u.username, 'photo', u."pictureURL") AS user, 
+  json_build_object('url', p.link, 'legend', p.description, 'title', NULL, 'description', NULL, 'image', NULL) AS link, 
+  p."createdAt" 
+  FROM posts p 
+  JOIN users u ON u.id = p."userId"
+  JOIN "hashtagsPosts" r ON r.postId = posts.id
+  JOIN hashtags h ON h.id = r.hashtagId.h.id
+  WHERE hashtags.name ILIKE $1
+  ORDER BY p."createdAt" DESC
+  LIMIT 20`,
+    [hashtag]
+  );
 }
 
 const hashtagsRepository = {
@@ -34,8 +47,8 @@ const hashtagsRepository = {
   getHashtagById,
   getHashtagIdByName,
   setUseCount,
-  deleteHashtag,
   insertHashtagPostRelation,
+  getHashtagPostByName,
 };
 
 export default hashtagsRepository;
--- src/routes/hashtagsRouter.js
+++ src/routes/hashtagsRouter.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { getPosts } from "../controllers/postsController.js";
-import getTrending from "../controllers/TrendingController.js";
+import getTrending from "../controllers/trendingController.js";
 import validateToken from "../middlewares/authValidator.js";
 
 const hashtagsRouter = Router();
 
-hashtagsRouter.het("/trending", validateToken, getTrending);
+hashtagsRouter.get("/trending", validateToken, getTrending);
 hashtagsRouter.get("/hashtag/:hashtag", validateToken, getPosts);
 
 export default hashtagsRouter;
