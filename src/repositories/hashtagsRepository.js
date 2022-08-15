ed30cac524c917bd43d5a0925ae5c453fc27a219
Lucas Lima <lugablima@gmail.com>
2022-08-15 14:37:02 -0300

    Add user id in getHashtagPostByName function query

--- src/repositories/hashtagsRepository.js
+++ src/repositories/hashtagsRepository.js
@@ -4,14 +4,6 @@ async function getHashtagByName(hashtag) {
   return db.query(`SELECT * FROM hashtags WHERE name = $1`, [hashtag]);
 }
 
-// async function getHashtagById(id) {
-//   return db.query(`SELECT * FROM hashtags WHERE id = $1`, [id]);
-// }
-
-// async function getHashtagIdByName(name) {
-//   return db.query(`SELECT hashtags.id FROM hashtags WHERE hashtags.name ILIKE $1 `, [name]);
-// }
-
 async function insertHashtag(hashtag) {
   return db.query(`INSERT INTO hashtags (name) VALUES ($1) RETURNING id`, [hashtag]);
 }
@@ -26,7 +18,7 @@ async function insertHashtagPostRelation(postId, hashtagId) {
 
 async function getHashtagPostByName(hashtag) {
   return db.query(
-    `SELECT p.id, json_build_object('name', u.username, 'photo', u."pictureURL") AS user, 
+    `SELECT p.id, json_build_object('id', u.id, 'name', u.username, 'photo', u."pictureURL") AS user, 
   json_build_object('url', p.link, 'legend', p.description, 'title', NULL, 'description', NULL, 'image', NULL) AS link, 
   p."createdAt" 
   FROM posts p 
@@ -43,8 +35,6 @@ async function getHashtagPostByName(hashtag) {
 const hashtagsRepository = {
   getHashtagByName,
   insertHashtag,
-  // getHashtagById,
-  // getHashtagIdByName,
   setUseCount,
   insertHashtagPostRelation,
   getHashtagPostByName,
