bf2eabc873a7e70ad59eabec41b90220e630f0f2
LucasLima <lugablima@gmail.com>
2022-08-14 23:09:14 -0300

    fix: adjust hashtags routes

--- src/controllers/hashtagsController.js
+++ src/controllers/hashtagsController.js
@@ -1,13 +1,30 @@
+import urlMetadata from "url-metadata";
 import hashtagsRepository from "../repositories/hashtagsRepository.js";
 
 export default async function getHashtags(req, res) {
   const { hashtag } = req.params;
   try {
-    const result = (await hashtagsRepository.getHashtagPostByName(hashtag)).rows;
-    if (result.length === 0) {
+    const { rows: hashtags } = await hashtagsRepository.getHashtagPostByName(hashtag);
+
+    if (hashtags.length === 0) {
       return res.status(404).send("No posts found with this hashtag");
     }
-    return res.send(result);
+
+    const newHashtags = await Promise.all(
+      hashtags.map(async (el) => {
+        const { title, image, description } = await urlMetadata(el.link.url);
+
+        const newHashtag = { ...el };
+
+        newHashtag.link.title = title;
+        newHashtag.link.image = image;
+        newHashtag.link.description = description;
+
+        return newHashtag;
+      })
+    );
+
+    res.status(200).send(newHashtags);
   } catch (error) {
     console.log(error);
     return res.status(500).send("Error trying to get posts by hashtags", error);
--- src/controllers/trendingController.js
+++ src/controllers/trendingController.js
@@ -2,11 +2,11 @@ import trendingRepository from "../repositories/trendingRepository.js";
 
 export default async function getTrending(req, res) {
   try {
-    const result = await trendingRepository.getTrending();
+    const result = await trendingRepository.getTrendingHashtags();
     if (result.rowCount === 0) {
       return res.status(404).send("No hashtags found.");
     }
-    return res.send(result.rows);
+    return res.status(200).send(result.rows);
   } catch (error) {
     console.log(error);
     return res.status(500).send("Error while trying to get trending hashtags", error);
